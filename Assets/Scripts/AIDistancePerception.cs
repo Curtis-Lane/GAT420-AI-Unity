84c050651fe51561174819b62fb782888454987a
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2024-01-09 14:25:28 -0700

    Preliminary work on AI agents

---
+++ Assets/Scripts/AIDistancePerception.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDistancePerception : AIPerception {
+    public override GameObject[] GetGameObjects() {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/AIPerception.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AIPerception : MonoBehaviour {
+	[SerializeField]
+	protected string tagName = "";
+	[SerializeField]
+	protected float distance = 1.0f;
+	[SerializeField]
+	protected float maxAngle = 45.0f;
+
+	public string TagName { get { return tagName; } }
+	public float Distance { get { return distance; } }
+	public float MaxAngle { get { return maxAngle; } }
+
+	public abstract GameObject[] GetGameObjects();
+}
